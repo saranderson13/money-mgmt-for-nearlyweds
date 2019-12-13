1e69a7a7f3e3d9ecd8c78dc1b7f8e8b394b67bc5
Sarah <saranderson13@gmail.com>
2019-12-12 20:59:25 -0500

    set up error handling for 401 & 404

--- money-mgmt-backend/app/controllers/application_controller.rb
+++ money-mgmt-backend/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
 
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from AuthenticationError, with: :unauthorized_error
+
     def render_resource(resource)
         if resource.errors.empty?
             render json: resource
@@ -21,6 +24,16 @@ class ApplicationController < ActionController::API
         }, status: :bad_request
     end
 
+    def unauthorized_error
+        render json: { message: "No access authorization." }, status: 404
+    end
 
+    def not_found
+        render json: { message: "Resource not found."}
+    end
+
+    def authorize_user_resource(resource)
+        raise AuthorizationError.new if resource.user != current_user
+    end
 
 end
